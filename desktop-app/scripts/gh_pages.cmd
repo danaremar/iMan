27230a6cfc68592cb43fde138bf1e7e5a3aafab5
danaremar <56022332+danaremar@users.noreply.github.com>
2022-06-06 01:16:37 +0200

    Frontend deployed

--- desktop-app/scripts/gh_pages.cmd
+++ desktop-app/scripts/gh_pages.cmd
@@ -1,6 +1,8 @@
 cd ..
 cd ..
 
-ng build --configuration=production
+ng build --configuration=production --base-href="./"
 git add desktop-app/dist -f && git commit -m "GitHub pages upload"
 git subtree push --prefix desktop-app/dist origin gh-pages
+
+@REM ng deploy --base-href=/iMan/
