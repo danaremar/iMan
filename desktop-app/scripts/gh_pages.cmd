c5ca583e4548802bbd137cdedfa5ef20898bbdd2
danaremar <56022332+danaremar@users.noreply.github.com>
2022-06-06 00:41:38 +0200

    Update gh_pages.cmd

--- desktop-app/scripts/gh_pages.cmd
+++ desktop-app/scripts/gh_pages.cmd
@@ -2,5 +2,5 @@ cd ..
 cd ..
 
 ng build --configuration=production
-git add ./desktop-app/dist && git commit -m "GitHub pages upload"
-git subtree push --prefix dist origin gh-pages
+git add desktop-app/dist -f && git commit -m "GitHub pages upload"
+git subtree push --prefix desktop-app/dist origin gh-pages
