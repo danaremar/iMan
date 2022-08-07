ba19752084fec02cc285fe2c34df6031c05977ef
danaremar <56022332+danaremar@users.noreply.github.com>
2022-08-07 20:57:24 +0200

    Common AgGrid class configuration

---
+++ desktop-app/src/app/services/ag-grid-api/ag-grid-api.ts
@@ -0,0 +1,30 @@
+import { HttpParams } from "@angular/common/http"
+
+export class AgGridApi {
+    public static getAgGridParams(pageNumber: number, pageSize: number, order: Array<any>, filter: any, incCol: any): HttpParams {
+
+        let params = new HttpParams()
+            .set("page", (pageNumber - 1).toString())
+            .set("size", pageSize.toString())
+
+        for (const a of order) {
+            params = params.append("sort", a.colId + ',' + a.sort)
+        }
+
+        if (filter) {
+            let b = this.getAllColumns(incCol)
+            for (let column of b) {
+                var searchFilter = filter[column]
+                if (searchFilter) {
+                    params = params.append(column, searchFilter.filter)
+                }
+            }
+        }
+
+        return params
+    }
+
+    public static getAllColumns(incCol: any): Array<string> {
+        return incCol.map((x: { field: any; }) => x.field)
+    }
+}
