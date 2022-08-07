369509ddca2b2d9abcccb9631bddaf605b22bd2e
danaremar <56022332+danaremar@users.noreply.github.com>
2022-08-07 20:57:42 +0200

    Update incidents.service.ts

--- desktop-app/src/app/services/incidents/incidents.service.ts
+++ desktop-app/src/app/services/incidents/incidents.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { IncidentCreateDto, IncidentShowDto, IncidentUpdateCreateDto, IncidentUpdateDto, IncidentUpdateShowDto } from "src/app/models/incidents/incidents";
 import { environment } from "src/environments/environment";
+import { AgGridApi } from "../ag-grid-api/ag-grid-api";
 
 @Injectable({
     providedIn: 'root'
@@ -23,60 +24,38 @@ export class IncidentService {
 
     // FIND
     public findIncidentsByProject(projectId: number, pageNumber: number, pageSize: number, order: Array<any>, filter: any, incCol: any): Observable<any> {
-        var url = this.hostUrl + 'project/' + projectId
-        let params = new HttpParams()
-            .set("page", (pageNumber - 1).toString())
-            .set("size", pageSize.toString())
-
-        for (let i = 0; i < order.length; i++) {
-            var a = order[i]
-            params = params.append("sort", a.colId + ',' + a.sort)
-        }
-
-        if (filter) {
-            var b = this.getAllColumns(incCol)
-            for (let column of b) {
-                var searchFilter = filter[column]
-                if (searchFilter) {
-                    params = params.append(column, searchFilter.filter)
-                }
-            }
-        }
-
+        let url = this.hostUrl + 'project/' + projectId
+        let params = AgGridApi.getAgGridParams(pageNumber, pageSize, order, filter, incCol)
         return this.httpClient.get<any>(url, { params })
-    }
-
-    public getAllColumns(incCol: any): Array<string> {
-        return incCol.map((x: { field: any; }) => x.field)
     }
 
     // GET INCIDENT FROM ID
     public getIncidentById(incidentId: number): Observable<any> {
-        var url = this.hostUrl + incidentId
+        let url = this.hostUrl + incidentId
         return this.httpClient.get<IncidentShowDto>(url)
     }
 
     // GET INCIDENT UPDATED FROM INCIDENT ID
     public getIncidentUpdatesByIncidentId(incidentId: number): Observable<any> {
-        var url = this.hostUrl + incidentId + '/updates'
+        let url = this.hostUrl + incidentId + '/updates'
         return this.httpClient.get<Array<IncidentUpdateShowDto>>(url)
     }
 
     // CREATE
     public createIncident(incidentCreateDto: IncidentCreateDto): Observable<any> {
-        var url = this.hostUrl
+        let url = this.hostUrl
         return this.httpClient.post<IncidentCreateDto>(url, incidentCreateDto)
     }
 
     // UPDATE
     public updateIncident(incidentUpdateDto: IncidentUpdateDto): Observable<any> {
-        var url = this.hostUrl
+        let url = this.hostUrl
         return this.httpClient.put<IncidentUpdateDto>(url, incidentUpdateDto)
     }
 
     // DISABLE
     public disableIncident(incidentId: number): Observable<any> {
-        var url = this.hostUrl + incidentId
+        let url = this.hostUrl + incidentId
         return this.httpClient.delete<any>(url)
     }
 
@@ -88,7 +67,7 @@ export class IncidentService {
 
     // CREATE
     public createIncidentUpdate(incidentId: number, incidentUpdateCreateDto: IncidentUpdateCreateDto): Observable<any> {
-        var url = this.hostUrl + incidentId + '/update'
+        let url = this.hostUrl + incidentId + '/update'
         return this.httpClient.post<IncidentUpdateCreateDto>(url, incidentUpdateCreateDto);
     }
 
