796d1e40312bc297cc8053d6dc909320fa266afd
danaremar <56022332+danaremar@users.noreply.github.com>
2022-12-10 01:26:22 +0100

    Electron package reduced

--- desktop-app/scripts/compile-win32-x64.cmd
+++ desktop-app/scripts/compile-win32-x64.cmd
@@ -1,3 +1,3 @@
 cd ..
-npm build --configuration=electronjs-prod
-npm exec electron-packager . --platform=win32 --arch=x64 --configuration=electronjs-prod --icon=src/favicon.ico
+ng build --configuration=electronjs-prod
+npm exec electron-packager . --platform=win32 --arch=x64 --configuration=electronjs-prod --icon=src/favicon.ico --out=pack
